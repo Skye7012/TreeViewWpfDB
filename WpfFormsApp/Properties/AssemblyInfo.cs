7d5ded82ef87a84be21b212e6a4106db7c768b44
skye7012 <skye7012@gmail.com>
2021-07-22 15:18:51 +0300

    assembly

--- WpfFormsApp/Properties/AssemblyInfo.cs
+++ WpfFormsApp/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("WpfFormsApp")]
+[assembly: AssemblyTitle("TreeViewWpfDB")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfFormsApp")]
+[assembly: AssemblyProduct("TreeViewWpfDB")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
